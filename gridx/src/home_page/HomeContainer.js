9631e2c2f024682a8c3caa5ce33755d904e814fd
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-03-31 23:32:17 +0100

    hero-section setting up

---
+++ gridx/src/home_page/HomeContainer.js
@@ -0,0 +1,13 @@
+import React from "react"
+import heroSection from "./heroSetion";
+
+
+function HomeConatiner(){
+  return (
+    <>
+      <heroSection/>
+    </>
+  )
+}
+
+export default HomeConatiner;
