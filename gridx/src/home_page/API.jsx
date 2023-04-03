1fd39a9563c33e625c7d2cb7edb32269b5dba725
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-04-03 21:04:30 +0100

    API section

--- gridx/src/home_page/API.jsx
+++ gridx/src/home_page/API.jsx
@@ -1,9 +1,28 @@
 import "./css/api-section.scss"
+import apiImage from "../icons/editor.svg"
 
 export default function API(){
   return(
     <div className="api-container">
-      Hey
+      <div className="first-wrapper">
+        <h4>API</h4>
+        <h3>Concentrate on the logic - not the infrastructure</h3>
+        <p>Use our API to build energy management applications from scratch and implement your own custom logic.</p>
+        <button>Get access</button>
+        <a href="/">Read the docs ›</a>
+
+        <div className="sub-first-wrapper">
+          <h3>Unified</h3>
+          <p>The API is well documented and provides clear and consistent data structures.</p>
+
+          <h3>RESTful</h3>
+          <p>All endpoints adhere to REST principles, making integration quick and seamless.</p>
+        </div>
+      </div>
+
+      <div className="second-wrapper">
+        <img src={apiImage} alt="/" />
+      </div>
     </div>
   )
 }
