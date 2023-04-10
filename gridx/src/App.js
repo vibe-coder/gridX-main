bbaedda57e8cd0eccf1af082e5483a107149aca0
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-04-10 18:23:07 +0100

    knowledge section set up

--- gridx/src/App.js
+++ gridx/src/App.js
@@ -550,7 +550,7 @@ function App() {
 
               <Link className='company-link'>
                 <div className='company-link-wrapper'>
-                  <div className='company-img-wrapper'><img src={virtualIcon} alt='/'/></div>
+                  <div className='company-img-wrapper'><img src={knowledgeIcon} alt='/'/></div>
                   <div className='company-text-wrapper'>
                     <h3>Knowledge</h3>
                   </div>
