85a21793bafe307ea6124b3658115af6b127e6ba
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-04-10 18:24:24 +0100

    Downloads section set up

--- gridx/src/App.js
+++ gridx/src/App.js
@@ -568,7 +568,7 @@ function App() {
 
               <Link className='company-link'>
                 <div className='company-link-wrapper'>
-                  <div className='company-img-wrapper'><img src={virtualIcon} alt='/'/></div>
+                  <div className='company-img-wrapper'><img src={downloadsIcon} alt='/'/></div>
                   <div className='company-text-wrapper'>
                     <h3>Downloads</h3>
                   </div>
